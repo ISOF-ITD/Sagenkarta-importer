282d9d0e762298f9f1d3aa7ab377fb4d144b74b4
Per Låås <per.laas@isof.se>
2021-10-21 08:52:36 +0200

    Log full body

--- test-node-request.js
+++ test-node-request.js
@@ -1,8 +1,11 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+//https://stackoverflow.com/questions/20433287/node-js-request-cert-has-expired
+//DANGEROUS This disables HTTPS / SSL / TLS checking across your entire node.js environment. 
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 request = require('request');
 
 request('https://frigg-test.isof.se/sagendatabas/api/records/1986/', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
+  console.log(body);
   console.log(body.url);
   console.log(body.explanation);
 });
